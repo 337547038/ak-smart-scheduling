e77e1bf2a2f5effc6231df3939c856d82cee7046
337547038 <337547038@qq.com>
2026-07-25 15:27:22 +0800

    docs

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 
 // 登录
 export function login(data) {
-  return request({ url: '/auth/login', method: 'get', data })
+  return request({ url: '/auth/login', method: 'post', data })
 }
 
 // 班级管理
